1e04db53c356c736d9a8aca65dcfdaae8aa6655d
Pushpendra Yadav <code.pushpendra@gmail.com>
2023-06-17 15:00:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from "react";
+
+function Square({ value, onSquareClick }) {
+  return <button className="square" onClick={onSquareClick}> {value} </button>;
+}
+
+function Board({xIsNext, squares, onPlay}) {
+  const [xIsNext, setxIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+
+    if(calculateWinner(squares)) {
+      alert('Game is Over Now');
+      return;
+    }
+
+    if(squares[i] != null) {
+      alert('You are allowed only once in an square');
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if(xIsNext) nextSquares[i] = 'X';
+    else nextSquares[i] = 'O';
+    setxIsNext(!xIsNext);
+    setSquares(nextSquares);
+  }
+
+  let winner = calculateWinner(squares), status;
+  if(winner) {
+    status = "winner " + winner;
+  } else status = "NEXT " + (xIsNext ? 'X' : 'O');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="status"> {status}  </div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => { handleClick(0) }} />
+        <Square value={squares[1]} onSquareClick={() => { handleClick(1) }} />
+        <Square value={squares[2]} onSquareClick={() => { handleClick(2) }} />
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => { handleClick(3) }} />
+        <Square value={squares[4]} onSquareClick={() => { handleClick(4) }} />
+        <Square value={squares[5]} onSquareClick={() => { handleClick(5) }} />
+      </div>
+
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => { handleClick(6) }} />
+        <Square value={squares[7]} onSquareClick={() => { handleClick(7) }} />
+        <Square value={squares[8]} onSquareClick={() => { handleClick(8) }} />
+      </div>
+    </>
+  );
+}
+
+export default function Game() {
+
+  const [xIsNext, setxIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol> {  } </ol>
+      </div>
     </div>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
